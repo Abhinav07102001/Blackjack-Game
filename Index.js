bb33a0fae48af927d65732e16fdd2030ae1afacc
Abhinav Lade <abhinavlade@gmail.com>
2024-01-26 23:33:03 +0530

    Js Code Modified

--- Index.js
+++ Index.js
@@ -1,45 +1,67 @@
-let firstCard=10;
-let secondCard=7;
-let sum=firstCard+secondCard;
+let cards=[];
+let sum=0;
 let hasBlackJack=false;
 let isAlive=true;
 let message="";
 
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el");
-let cardEl=document.querySelector("#card-el");
+let cardEl=document.getElementById("card-el");
+
+function getRandomCard()
+{
+    let Value=Math.floor(Math.random()*13)+1;
+    if(Value===1)return 11;
+    else if(Value>=11&&Value<=13)return 10;
+    else return Value;
+}
 
 function startGame()
-{
-    renderGame()
+{ 
+    isAlive=true;
+    hasBlackJack=false;
+    let firstCard=getRandomCard();
+    let secondCard=getRandomCard();
+    cards=[firstCard,secondCard];
+    sum=firstCard+secondCard;
+    renderGame();
 }
 
 function renderGame()
 {
-    if(sum<21)
-    {
-        message="Do you want to draw a new card? 🙂"
-    }
+    if(sum<21)message="Do you want to draw a new card? 🙂";
     else if(sum===21)
     {
-        message="Wohoo! You've got Blackjack! 🥳"
-        hasBlackJack = true
+        message="Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
     }
     else if(sum>21)
     {
-        message="You're out of the game! 😭"
-        isAlive=false
+        message="You're out of the game! 😭";
+        isAlive=false;
     }
 
     messageEl.textContent=message;
     sumEl.textContent="Sum: "+sum;
-    cardEl.textContent="Cards: "+firstCard+" "+secondCard;
+    cardEl.textContent="Cards: ";
+    
+    for(let i=0;i<cards.length;i++)
+    {
+        cardEl.textContent+=cards[i]+" ";
+    }
+    
 }  
-
 
 function newCard()
 {
-    let newCard=4;
-    sum+=newCard;
-    startGame();
+    if(isAlive&&!hasBlackJack)
+    {
+        let newCard=getRandomCard();
+        sum+=newCard;
+        cards.push(newCard);
+        renderGame();
+    }   
 }
+
+
+
